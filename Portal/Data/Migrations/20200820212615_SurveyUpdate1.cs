62a4b69983781dbfdda2eb8faf53ae1d1aaa71f3
wood71 <wood71@DESKTOP-E4AHLAJ>
2020-08-20 12:17:32 -0500

    change

---
+++ Portal/Data/Migrations/20200820212615_SurveyUpdate1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Portal.Data.Migrations
+{
+    public partial class SurveyUpdate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionID",
+                table: "Choices",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Choices_QuestionID",
+                table: "Choices",
+                column: "QuestionID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Choices_Questions_QuestionID",
+                table: "Choices",
+                column: "QuestionID",
+                principalTable: "Questions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Choices_Questions_QuestionID",
+                table: "Choices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Choices_QuestionID",
+                table: "Choices");
+
+            migrationBuilder.DropColumn(
+                name: "QuestionID",
+                table: "Choices");
+        }
+    }
+}
