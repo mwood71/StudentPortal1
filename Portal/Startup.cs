66f873d9937e51a7c5bab2f4792d759529f9ce0a
wood71 <wood71@DESKTOP-E4AHLAJ>
2020-08-22 18:06:45 -0500

    completion

--- Portal/Startup.cs
+++ Portal/Startup.cs
@@ -30,7 +30,7 @@ namespace Portal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddScoped<IDbInitializer, DbInitializer>();
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
@@ -44,7 +44,7 @@ namespace Portal
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDbInitializer dbInitializer)
         {
             if (env.IsDevelopment())
             {
@@ -58,6 +58,7 @@ namespace Portal
                 app.UseHsts();
             }
 
+            dbInitializer.Initialize();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
